7cbd34c77e2deb537c0cd1eef003c39f4221e1da
NHLGames <muricajoe@gmail.com>
2016-03-25 16:35:37 -0400

    Fixed conflict
    Woops, accidentally created a conflict.  Fixed now

--- WindowsApplication1/Utilities/Downloader.vb
+++ WindowsApplication1/Utilities/Downloader.vb
@@ -15,14 +15,6 @@ Public Class Downloader
     Private Shared LocalFileDirectory = Application.StartupPath
 
     Private Shared Sub DownloadFile(URL As String, fileName As String, Optional checkIfExists As Boolean = False, Optional overwrite As Boolean = True)
-
-<<<<<<< HEAD
-        Dim fullPath As String = LocalFileDirectory & "\" & fileName
-
-        If (checkIfExists = False) OrElse (checkIfExists AndAlso My.Computer.FileSystem.FileExists(fullPath) = False) Then
-            Console.WriteLine("Downloading file: " & URL & " to " & fullPath)
-            My.Computer.Network.DownloadFile(URL, fullPath, "", "", False, 10000, overwrite)
-=======
         Dim fullPath As String = Combine(LocalFileDirectory, fileName)
 
         If (checkIfExists = False) OrElse (checkIfExists AndAlso My.Computer.FileSystem.FileExists(fullPath) = False) Then
@@ -32,21 +24,11 @@ Public Class Downloader
             Catch ex As Exception
                 Console.WriteLine("Error: " & ex.Message)
             End Try
->>>>>>> origin/master
         End If
-
     End Sub
 
     Private Shared Function ReadFileContents(fileName As String) As String
-
         Dim returnValue As String = ""
-<<<<<<< HEAD
-        Dim filePath As String = LocalFileDirectory & "\" & fileName
-
-        Using streamReader As IO.StreamReader = New IO.StreamReader(filePath)
-            returnValue = streamReader.ReadToEnd()
-        End Using
-=======
         Dim filePath As String = Combine(LocalFileDirectory, fileName)
         Try
             Using streamReader As IO.StreamReader = New IO.StreamReader(filePath)
@@ -55,7 +37,6 @@ Public Class Downloader
         Catch ex As Exception
             Console.WriteLine("Error: " & ex.Message)
         End Try
->>>>>>> origin/master
 
         Return returnValue
     End Function
