f6ce5f79102f65a3d0a9e0511ce6fa4e06f47d2b
J M <jeffreymackenzie@gmail.com>
2016-03-25 09:45:41 -0400

    Add MainMetroForm

---
+++ WindowsApplication1/Controls/GameControl.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GameControl
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BorderPanel1 = New BorderPanel()
+        Me.picHome = New System.Windows.Forms.PictureBox()
+        Me.lblTime = New MetroFramework.Controls.MetroLabel()
+        Me.picAway = New System.Windows.Forms.PictureBox()
+        Me.btnWatch = New MetroFramework.Controls.MetroButton()
+        Me.lblVS = New MetroFramework.Controls.MetroLabel()
+        Me.lblAwayScore = New MetroFramework.Controls.MetroLabel()
+        Me.lblHomeTeam = New MetroFramework.Controls.MetroLabel()
+        Me.lblHomeScore = New MetroFramework.Controls.MetroLabel()
+        Me.lblAwayTeam = New MetroFramework.Controls.MetroLabel()
+        Me.BorderPanel1.SuspendLayout()
+        CType(Me.picHome, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picAway, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BorderPanel1
+        '
+        Me.BorderPanel1.BorderColour = System.Drawing.Color.LightGray
+        Me.BorderPanel1.Controls.Add(Me.picHome)
+        Me.BorderPanel1.Controls.Add(Me.lblTime)
+        Me.BorderPanel1.Controls.Add(Me.picAway)
+        Me.BorderPanel1.Controls.Add(Me.btnWatch)
+        Me.BorderPanel1.Controls.Add(Me.lblVS)
+        Me.BorderPanel1.Controls.Add(Me.lblAwayScore)
+        Me.BorderPanel1.Controls.Add(Me.lblHomeTeam)
+        Me.BorderPanel1.Controls.Add(Me.lblHomeScore)
+        Me.BorderPanel1.Controls.Add(Me.lblAwayTeam)
+        Me.BorderPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.BorderPanel1.Name = "BorderPanel1"
+        Me.BorderPanel1.Size = New System.Drawing.Size(300, 95)
+        Me.BorderPanel1.TabIndex = 9
+        '
+        'picHome
+        '
+        Me.picHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.picHome.Location = New System.Drawing.Point(7, 13)
+        Me.picHome.Name = "picHome"
+        Me.picHome.Size = New System.Drawing.Size(75, 50)
+        Me.picHome.TabIndex = 0
+        Me.picHome.TabStop = False
+        '
+        'lblTime
+        '
+        Me.lblTime.Location = New System.Drawing.Point(89, 5)
+        Me.lblTime.Name = "lblTime"
+        Me.lblTime.Size = New System.Drawing.Size(121, 19)
+        Me.lblTime.TabIndex = 8
+        Me.lblTime.Text = "7:30 PM"
+        Me.lblTime.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'picAway
+        '
+        Me.picAway.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.picAway.Location = New System.Drawing.Point(216, 13)
+        Me.picAway.Name = "picAway"
+        Me.picAway.Size = New System.Drawing.Size(75, 50)
+        Me.picAway.TabIndex = 1
+        Me.picAway.TabStop = False
+        '
+        'btnWatch
+        '
+        Me.btnWatch.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnWatch.FontWeight = MetroFramework.MetroButtonWeight.Regular
+        Me.btnWatch.Location = New System.Drawing.Point(108, 61)
+        Me.btnWatch.Name = "btnWatch"
+        Me.btnWatch.Size = New System.Drawing.Size(80, 23)
+        Me.btnWatch.TabIndex = 7
+        Me.btnWatch.Text = "Watch"
+        Me.btnWatch.UseSelectable = True
+        '
+        'lblVS
+        '
+        Me.lblVS.AutoSize = True
+        Me.lblVS.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.lblVS.Location = New System.Drawing.Point(137, 31)
+        Me.lblVS.Name = "lblVS"
+        Me.lblVS.Size = New System.Drawing.Size(26, 19)
+        Me.lblVS.TabIndex = 2
+        Me.lblVS.Text = "VS"
+        '
+        'lblAwayScore
+        '
+        Me.lblAwayScore.AutoSize = True
+        Me.lblAwayScore.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.lblAwayScore.Location = New System.Drawing.Point(171, 31)
+        Me.lblAwayScore.Name = "lblAwayScore"
+        Me.lblAwayScore.Size = New System.Drawing.Size(17, 19)
+        Me.lblAwayScore.TabIndex = 6
+        Me.lblAwayScore.Text = "0"
+        '
+        'lblHomeTeam
+        '
+        Me.lblHomeTeam.Location = New System.Drawing.Point(7, 66)
+        Me.lblHomeTeam.Name = "lblHomeTeam"
+        Me.lblHomeTeam.Size = New System.Drawing.Size(80, 19)
+        Me.lblHomeTeam.TabIndex = 3
+        Me.lblHomeTeam.Text = "Home Team"
+        Me.lblHomeTeam.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblHomeScore
+        '
+        Me.lblHomeScore.AutoSize = True
+        Me.lblHomeScore.FontWeight = MetroFramework.MetroLabelWeight.Bold
+        Me.lblHomeScore.Location = New System.Drawing.Point(108, 31)
+        Me.lblHomeScore.Name = "lblHomeScore"
+        Me.lblHomeScore.Size = New System.Drawing.Size(17, 19)
+        Me.lblHomeScore.TabIndex = 5
+        Me.lblHomeScore.Text = "0"
+        '
+        'lblAwayTeam
+        '
+        Me.lblAwayTeam.Location = New System.Drawing.Point(216, 66)
+        Me.lblAwayTeam.Name = "lblAwayTeam"
+        Me.lblAwayTeam.Size = New System.Drawing.Size(75, 19)
+        Me.lblAwayTeam.TabIndex = 4
+        Me.lblAwayTeam.Text = "Away Team"
+        Me.lblAwayTeam.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'GameControl
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.Controls.Add(Me.BorderPanel1)
+        Me.Name = "GameControl"
+        Me.Padding = New System.Windows.Forms.Padding(10)
+        Me.Size = New System.Drawing.Size(301, 97)
+        Me.BorderPanel1.ResumeLayout(False)
+        Me.BorderPanel1.PerformLayout()
+        CType(Me.picHome, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picAway, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picHome As PictureBox
+    Friend WithEvents picAway As PictureBox
+    Friend WithEvents lblVS As MetroFramework.Controls.MetroLabel
+    Friend WithEvents lblHomeTeam As MetroFramework.Controls.MetroLabel
+    Friend WithEvents lblAwayTeam As MetroFramework.Controls.MetroLabel
+    Friend WithEvents lblHomeScore As MetroFramework.Controls.MetroLabel
+    Friend WithEvents lblAwayScore As MetroFramework.Controls.MetroLabel
+    Friend WithEvents btnWatch As MetroFramework.Controls.MetroButton
+    Friend WithEvents lblTime As MetroFramework.Controls.MetroLabel
+    Friend WithEvents BorderPanel1 As BorderPanel
+End Class
---
+++ WindowsApplication1/NHLGamesMetro.vb
@@ -0,0 +1,296 @@
+﻿Imports System.Globalization
+Imports System.IO
+Imports System.Security.Permissions
+Imports System.Threading
+Imports MetroFramework
+Imports MetroFramework.Forms
+Imports Newtonsoft.Json.Linq
+Imports NHLGames.TextboxConsoleOutputRediect
+
+Public Class NHLGamesMetro
+
+    Private AvailableGames As New List(Of String)
+    Private Const ServerIP As String = "146.185.131.14"
+    Private Const DomainName As String = "mf.svc.nhl.com"
+
+    Public Shared FormInstance As NHLGamesMetro = Nothing
+
+    ' Starts the application. -- See: https://msdn.microsoft.com/en-us/library/system.windows.forms.application.threadexception(v=vs.110).aspx
+    <SecurityPermission(SecurityAction.Demand, Flags:=SecurityPermissionFlag.ControlAppDomain)>
+    Public Shared Sub Main()
+        ' Add the event handler for handling UI thread exceptions to the event.
+        AddHandler Application.ThreadException, AddressOf Form1_UIThreadException
+
+        ' Set the unhandled exception mode to force all Windows Forms errors to go through our handler.
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException)
+
+        ' Add the event handler for handling non-UI thread exceptions to the event. 
+        AddHandler AppDomain.CurrentDomain.UnhandledException, AddressOf CurrentDomain_UnhandledException
+
+        Dim form As New NHLGamesMetro()
+        FormInstance = form
+
+        'Setup redirecting console.out to 
+        Dim _writer = New TextBoxStreamWriter(form.RichTextBox)
+        Console.SetOut(_writer)
+
+        '' Runs the application.
+        Application.Run(form)
+    End Sub
+
+    Private Sub IntitializeApplicationSettings()
+
+        Dim mpcPath As String = ApplicationSettings.Read(Of String)(ApplicationSettings.Settings.MPCPath, "")
+        If mpcPath = "" Then
+            mpcPath = FileAccess.LocateEXE("mpc-hc64.exe", "\MPC-HC")
+            If mpcPath = String.Empty Then
+                mpcPath = "Unknown"
+            Else
+                ApplicationSettings.SetValue(ApplicationSettings.Settings.MPCPath, mpcPath)
+            End If
+        End If
+
+        txtMPCPath.Text = mpcPath
+
+
+        Dim vlcPath As String = ApplicationSettings.Read(Of String)(ApplicationSettings.Settings.VLCPath, "")
+        If vlcPath = "" Then
+            vlcPath = FileAccess.LocateEXE("vlc.exe", "\VideoLAN\VLC")
+            If vlcPath = String.Empty Then
+                vlcPath = "Unknown"
+            Else
+                ApplicationSettings.SetValue(ApplicationSettings.Settings.VLCPath, vlcPath)
+            End If
+        End If
+        txtVLCPath.Text = vlcPath
+
+
+        Dim liveStreamerPath As String = ApplicationSettings.Read(Of String)(ApplicationSettings.Settings.LiveStreamerPath, "")
+        If liveStreamerPath = "" Then
+            liveStreamerPath = Application.StartupPath & "\livestreamer-v1.12.2\livestreamer.exe"
+            ApplicationSettings.SetValue(ApplicationSettings.Settings.LiveStreamerPath, liveStreamerPath)
+        End If
+        txtLiveStreamPath.Text = liveStreamerPath
+
+        MetroCheckBox1.Checked = ApplicationSettings.Read(Of Boolean)(ApplicationSettings.Settings.ShowScores, True)
+
+
+    End Sub
+
+    ' Handle the UI exceptions by showing a dialog box, and asking the user whether
+    ' or not they wish to abort execution.
+    Private Shared Sub Form1_UIThreadException(ByVal sender As Object, ByVal t As ThreadExceptionEventArgs)
+        Console.WriteLine("Error: " & t.Exception.ToString())
+    End Sub
+
+    Private Shared Sub CurrentDomain_UnhandledException(ByVal sender As Object, ByVal e As UnhandledExceptionEventArgs)
+        Console.WriteLine(e.ExceptionObject.ToString())
+
+    End Sub
+
+    Public Sub HandleException(e As Exception)
+
+        Console.WriteLine(e.ToString())
+
+    End Sub
+    Private Sub VersionCheck()
+
+        Dim strLatest = Downloader.DownloadApplicationVersion()
+        Console.WriteLine("Status: Current version is " & strLatest)
+        Dim versionFromSettings = ApplicationSettings.Read(Of String)(ApplicationSettings.Settings.Version, "")
+        If strLatest > versionFromSettings Then
+            lblVersion.Text = "Version " & strLatest & " available! You are running " & versionFromSettings & "."
+            lblVersion.ForeColor = Color.Red
+            MetroMessageBox.Show(Me, "You don't have the latest version of this application. Things may not work properly (or at all).", "New Version Available", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            'lblVersion.Text = "Up to date! You are running " & settingsReader.GetValue("version", GetType(String)) & "."
+            'lblVersion.ForeColor = Color.Green
+        End If
+    End Sub
+
+    Public Sub NewGameFoundHandler(gameObj As Game)
+
+        Dim gameControl As New GameControl(gameObj, ApplicationSettings.Read(Of Boolean)(ApplicationSettings.Settings.ShowScores, True))
+        FlowLayoutPanel.Controls.Add(gameControl)
+
+    End Sub
+
+
+    Private Sub NHLGames_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AddHandler GameManager.NewGameFound, AddressOf NewGameFoundHandler
+
+        dtDate.Value = DateHelper.GetCentralTime()
+        dtDate.MaxDate = DateHelper.GetCentralTime()
+        TabControl.SelectedIndex = 0
+
+        If (HostsFile.TestEntry(DomainName, ServerIP) = False) Then
+            HostsFile.AddEntry(ServerIP, DomainName)
+        End If
+
+        VersionCheck()
+        IntitializeApplicationSettings()
+
+
+    End Sub
+
+
+
+    Private Sub dtDate_ValueChanged(sender As Object, e As EventArgs) Handles dtDate.ValueChanged
+
+        LoadGames(dtDate.Value)
+    End Sub
+
+    Private Sub LoadGames(dateTime As DateTime)
+
+        Try
+
+            'If dateTime <> GameManager.GamesListDate Then
+            GameManager.ClearGames()
+                FlowLayoutPanel.Controls.Clear()
+            'End If
+
+            Dim JSONSchedule As JObject = Downloader.DownloadJSONSchedule(dateTime)
+            AvailableGames = Downloader.DownloadAvailableGames() 'TODO: not download each time?
+            GameManager.RefreshGames(dateTime, JSONSchedule, AvailableGames)
+
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        LoadGames(dtDate.Value)
+    End Sub
+
+
+
+    Private Sub btnWatch_Click(sender As Object, e As EventArgs)
+        'If rbLive.Checked Then
+        '    WatchGame(False)
+        'Else
+        '    WatchGame(True)
+        'End If
+    End Sub
+
+
+    'Private Sub WatchGame(isVOD As Boolean, game As Game, streamType As GameStream.StreamType)
+
+    '    Dim args As New Game.GameWatchArguments
+    '    args.IsVOD = isVOD
+    '    Dim cdn As String = String.Empty
+
+
+    '    If rbLevel3.Checked Then
+    '        args.CDN = "l3c"
+    '    ElseIf rbAkamai.Checked Then
+    '        args.CDN = "akc"
+    '    End If
+
+    '    If rbQual1.Checked Then
+    '        args.Quality = "224p"
+    '    ElseIf rbQual2.Checked Then
+    '        args.Quality = "288p"
+    '    ElseIf rbQual3.Checked Then
+    '        args.Quality = "360p"
+    '    ElseIf rbQual4.Checked Then
+    '        args.Quality = "504p"
+    '    ElseIf rbQual6.Checked Then
+    '        args.Quality = "540p"
+    '    ElseIf rbQual6.Checked Then
+    '        args.Quality = "720p"
+    '        If chk60.Checked Then
+    '            args.Is60FPS = True
+    '            args.Quality = "best"
+    '        End If
+    '    End If
+
+    '    If streamType = GameStream.StreamType.Away Then
+    '        args.Stream = game.AwayStream
+    '    ElseIf streamType = GameStream.StreamType.Home Then
+    '        args.Stream = game.HomeStream
+    '    ElseIf streamType = GameStream.StreamType.National Then
+    '        args.Stream = game.NationalStream
+    '    ElseIf streamType = GameStream.StreamType.French Then
+    '        args.Stream = game.FrenchStream
+    '    End If
+
+    '    If rbMPC.Checked Then
+    '        args.PlayerPath = FileAccess.LocateEXE("mpc-hc64.exe", "\MPC-HC")
+    '    Else
+    '        args.PlayerPath = FileAccess.LocateEXE("vlc.exe", "\VideoLAN\VLC")
+    '    End If
+
+
+    '    Task.Run(Of Boolean)((Function()
+    '                              game.Watch(args)
+    '                              Return True
+    '                          End Function))
+
+
+    'End Sub
+
+    Private Sub RichTextBox_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox.TextChanged
+        RichTextBox.SelectionStart = RichTextBox.Text.Length
+        RichTextBox.ScrollToCaret()
+    End Sub
+
+
+
+    Private Sub btnOpenHostsFile_Click(sender As Object, e As EventArgs) Handles btnOpenHostsFile.Click
+        Dim HostsFilePath As String = Environment.SystemDirectory & "\drivers\etc\hosts"
+        Process.Start(HostsFilePath)
+    End Sub
+
+    Private Sub btnVLCPath_Click(sender As Object, e As EventArgs) Handles btnVLCPath.Click
+
+        OpenFileDialog.Filter = "VLC|vlc.exe"
+        OpenFileDialog.Multiselect = False
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            If String.IsNullOrEmpty(OpenFileDialog.FileName) = False Then
+                ApplicationSettings.SetValue(ApplicationSettings.Settings.VLCPath, OpenFileDialog.FileName)
+            End If
+
+        End If
+    End Sub
+
+    Private Sub btnMPCPath_Click(sender As Object, e As EventArgs) Handles btnMPCPath.Click
+        OpenFileDialog.Filter = "MPC|mpc-hc64.exe"
+        OpenFileDialog.Multiselect = False
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            If String.IsNullOrEmpty(OpenFileDialog.FileName) = False Then
+                ApplicationSettings.SetValue(ApplicationSettings.Settings.MPCPath, OpenFileDialog.FileName)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnLiveStreamerPath_Click(sender As Object, e As EventArgs) Handles btnLiveStreamerPath.Click
+        OpenFileDialog.Filter = "LiveStreamer|livestreamer.exe"
+        OpenFileDialog.Multiselect = False
+        If OpenFileDialog.ShowDialog() = DialogResult.OK Then
+            If String.IsNullOrEmpty(OpenFileDialog.FileName) = False Then
+                ApplicationSettings.SetValue(ApplicationSettings.Settings.LiveStreamerPath, OpenFileDialog.FileName)
+            End If
+        End If
+    End Sub
+
+    Private Sub MetroCheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles MetroCheckBox1.CheckedChanged
+        ApplicationSettings.SetValue(ApplicationSettings.Settings.ShowScores, MetroCheckBox1.Checked)
+    End Sub
+
+    Private Sub MetroTrackBar1_Scroll(sender As Object, e As ScrollEventArgs)
+        'lblRefreshValue.Text = MetroTrackBar1.Value & " Minutes"
+        'ApplicationSettings.SetValue(ApplicationSettings.Settings.RefreshIntervalInMin, MetroTrackBar1.Value)
+    End Sub
+
+    Private Sub btnClearConsole_Click(sender As Object, e As EventArgs) Handles btnClearConsole.Click
+        RichTextBox.Clear()
+    End Sub
+
+    Private Sub btnHosts_Click(sender As Object, e As EventArgs) Handles btnHosts.Click
+        If HostsFile.TestEntry(DomainName, ServerIP) Then
+            MetroMessageBox.Show(Me, "Hosts file looks good!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MetroMessageBox.Show(Me, "Hosts entry doesn't seem to be working :(", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
