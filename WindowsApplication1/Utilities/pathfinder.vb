e2556ce95d85faae5da99ab657b8870f3ed8545e
Jose Ouellet <joseouellet@gmail.com>
2017-04-20 18:33:27 -0400

    fix vlc.exe path ?

--- WindowsApplication1/Utilities/pathfinder.vb
+++ WindowsApplication1/Utilities/pathfinder.vb
@@ -2,11 +2,8 @@
 
 Public Class PathFinder
     Public Shared Function GetPathOfVLC() As String
-        Dim vlcValue = My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC", "InstallDir", Nothing)
-        If vlcValue.Substring(vlcValue.Length - 4, 4) <> ".exe" Then
-            vlcValue += "\vlc.exe"
-        End If
-        Return vlcValue
+        Return My.Computer.Registry.GetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\vlc.exe",
+                                             "", "C:\Program Files\VideoLAN\VLC\vlc.exe")
     End Function
 
     Public Shared Function GetPathOfMPC() As String
