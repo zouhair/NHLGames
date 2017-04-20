fc71d39ae0cede9a352ae8f60e76e5a22dde6ab1
Jose Ouellet <joseouellet@gmail.com>
2017-04-19 20:40:26 -0400

    path finder for vlc.exe and mpc.exe based on regedit
    it should work

---
+++ WindowsApplication1/Utilities/pathfinder.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.Win32
+
+Public Class PathFinder
+    Public Shared Function GetPathOfVLC() As String
+        Dim vlcValue = My.Computer.Registry.GetValue(
+            "HKEY_LOCAL_MACHINE\SOFTWARE\VideoLAN\VLC", "InstallDir", Nothing)
+        If vlcValue.Substring(vlcValue.Length - 4, 4) <> ".exe" Then
+            vlcValue += "\vlc.exe"
+        End If
+        Return vlcValue
+    End Function
+
+    Public Shared Function GetPathOfMPC() As String
+        Return My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\MPC-HC\MPC-HC", "ExePath", Nothing)
+    End Function
+
+End Class
