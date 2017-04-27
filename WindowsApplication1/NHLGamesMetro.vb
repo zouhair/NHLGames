14a3b79be7a9e12052615b404d3d5873ef7e1899
Jose Ouellet <joseouellet@gmail.com>
2017-04-26 22:48:52 -0400

    added close button, when UI freezes Metro does not show a close button

--- WindowsApplication1/NHLGamesMetro.vb
+++ WindowsApplication1/NHLGamesMetro.vb
@@ -656,5 +656,9 @@ Public Class NHLGamesMetro
         SetEventArgsFromForm()
     End Sub
 
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
 #End Region
 End Class
