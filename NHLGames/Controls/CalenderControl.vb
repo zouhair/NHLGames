5992e05a346cc409dfe05fb715aa57ddc14eeee6
jwallet <joseouellet@gmail.com>
2017-10-23 00:35:32 -0400

    calendar recoded

--- NHLGames/Controls/CalenderControl.vb
+++ NHLGames/Controls/CalenderControl.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Globalization
+Imports System.Web.UI.WebControls.Expressions
 Imports NHLGames.Utilities
 
 Namespace Controls
@@ -6,6 +7,7 @@ Namespace Controls
     Public Class CalenderControl
 
         Private _currentDate As Date
+        Private ReadOnly _arrayButtons(,) As Button
 
         Public Sub ReloadCal(ByVal ldate As Date, ByVal selected As Integer)
             _currentDate = ldate
@@ -21,168 +23,44 @@ Namespace Controls
             Sat.Text = DateHelper.GetFormattedWeek(6)
             Dim fdate As DayOfWeek = GetFirstOfMonthDay(ldate)
             Dim idate As Integer = 1
-            Dim row As Integer = 1
-            Dim col As Integer = 0
-            Do
-                If col >= fdate Then
-                    Exit Do
-                End If
-                GetButton(col, row).Visible = False
-                col += 1
-            Loop
-            Do
-                GetButton(fdate, row).Text = idate
-                GetButton(fdate, row).ForeColor = Color.Black
-                GetButton(fdate, row).BackColor = Color.White
+            Dim row As Integer = 0
+
+            For c As Integer = 0 To fdate - 1
+                _arrayButtons(row, c).Enabled = False
+            Next
+
+            While idate < Date.DaysInMonth(ldate.Year, ldate.Month) + 1
+                _arrayButtons(row, fdate).Text = idate
+                _arrayButtons(row, fdate).ForeColor = Color.Black
+                _arrayButtons(row, fdate).BackColor = Color.White
                 If idate = selected And ldate.Month = Date.Today.Month And ldate.Year = Date.Today.Year Then
-                    GetButton(fdate, row).ForeColor = Color.White
-                    GetButton(fdate, row).BackColor = Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(210, Byte), Integer))
+                    _arrayButtons(row, fdate).ForeColor = Color.White
+                    _arrayButtons(row, fdate).BackColor = Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(210, Byte), Integer))
                 End If
                 If fdate = DayOfWeek.Saturday Then
                     row += 1
                 End If
-                fdate = tdate(fdate)
+                fdate = If(fdate = 6, 0, fdate +1)
                 idate += 1
-                If idate = Date.DaysInMonth(ldate.Year, ldate.Month) + 1 Then
-                    Exit Do
-                End If
-            Loop
-            Do
-                If (idate + GetFirstOfMonthDay(ldate)) > 42 Then
-                    Exit Do
-                End If
-                GetButton(fdate, row).Visible = False
+            End While
+
+            While (idate + GetFirstOfMonthDay(ldate)) <= 42
+                _arrayButtons(row, fdate).Enabled = False
                 If fdate = DayOfWeek.Saturday Then
                     row += 1
                 End If
-                fdate = tdate(fdate)
+                fdate = If(fdate = 6, 0, fdate +1)
                 idate += 1
-            Loop
+            End While
+
         End Sub
 
         Sub Clearall()
-            For Each txt In {
-                                Su1, Su2, Su3, Su4, Su5, Su6,
-                                Mo1, Mo2, Mo3, Mo4, Mo5, Mo6,
-                                Tu1, Tu2, Tu3, Tu4, Tu5, Tu6,
-                                We1, We2, We3, We4, We5, We6,
-                                Th1, Th2, Th3, Th4, Th5, Th6,
-                                Fr1, Fr2, Fr3, Fr4, Fr5, Fr6,
-                                Sa1, Sa2, Sa3, Sa4, Sa5, Sa6}
-                txt.Text = Nothing
-                txt.Visible = True
+            For Each bt In _arrayButtons
+                bt.Text = Nothing
+                bt.Enabled = True
             Next
         End Sub
-
-        Function GetButton(ByVal day As DayOfWeek, ByVal row As Integer) As Button
-            Select Case day
-                Case DayOfWeek.Sunday
-                    Select Case row
-                        Case 1
-                            Return Su1
-                        Case 2
-                            Return Su2
-                        Case 3
-                            Return Su3
-                        Case 4
-                            Return Su4
-                        Case 5
-                            Return Su5
-                        Case Else
-                            Return Su6
-                    End Select
-                Case DayOfWeek.Monday
-                    Select Case row
-                        Case 1
-                            Return Mo1
-                        Case 2
-                            Return Mo2
-                        Case 3
-                            Return Mo3
-                        Case 4
-                            Return Mo4
-                        Case 5
-                            Return Mo5
-                        Case Else
-                            Return Mo6
-                    End Select
-                Case DayOfWeek.Tuesday
-                    Select Case row
-                        Case 1
-                            Return Tu1
-                        Case 2
-                            Return Tu2
-                        Case 3
-                            Return Tu3
-                        Case 4
-                            Return Tu4
-                        Case 5
-                            Return Tu5
-                        Case Else
-                            Return Tu6
-                    End Select
-                Case DayOfWeek.Wednesday
-                    Select Case row
-                        Case 1
-                            Return We1
-                        Case 2
-                            Return We2
-                        Case 3
-                            Return We3
-                        Case 4
-                            Return We4
-                        Case 5
-                            Return We5
-                        Case Else
-                            Return We6
-                    End Select
-                Case DayOfWeek.Thursday
-                    Select Case row
-                        Case 1
-                            Return Th1
-                        Case 2
-                            Return Th2
-                        Case 3
-                            Return Th3
-                        Case 4
-                            Return Th4
-                        Case 5
-                            Return Th5
-                        Case Else
-                            Return Th6
-                    End Select
-                Case DayOfWeek.Friday
-                    Select Case row
-                        Case 1
-                            Return Fr1
-                        Case 2
-                            Return Fr2
-                        Case 3
-                            Return Fr3
-                        Case 4
-                            Return Fr4
-                        Case 5
-                            Return Fr5
-                        Case Else
-                            Return Fr6
-                    End Select
-                Case Else
-                    Select Case row
-                        Case 1
-                            Return Sa1
-                        Case 2
-                            Return Sa2
-                        Case 3
-                            Return Sa3
-                        Case 4
-                            Return Sa4
-                        Case 5
-                            Return Sa5
-                        Case Else
-                            Return Sa6
-                    End Select
-            End Select
-        End Function
 
         Private Function GetFirstOfMonthDay(ByVal thisDay As Date) As DayOfWeek
             Dim tday As DayOfWeek = thisDay.DayOfWeek
@@ -192,56 +70,22 @@ Namespace Controls
             End If
             Do
                 tint -= 1
-                tday = ydate(tday)
+                tday = If(tday = 0, 6, tday -1)
                 If tint = 1 Then Exit Do
             Loop
             Return tday
         End Function
 
-        Private Function ydate(ByVal tday As DayOfWeek) As DayOfWeek
-            Dim rday As DayOfWeek
-            Select Case tday
-                Case DayOfWeek.Sunday
-                    rday = DayOfWeek.Saturday
-                Case DayOfWeek.Monday
-                    rday = DayOfWeek.Sunday
-                Case DayOfWeek.Tuesday
-                    rday = DayOfWeek.Monday
-                Case DayOfWeek.Wednesday
-                    rday = DayOfWeek.Tuesday
-                Case DayOfWeek.Thursday
-                    rday = DayOfWeek.Wednesday
-                Case DayOfWeek.Friday
-                    rday = DayOfWeek.Thursday
-                Case DayOfWeek.Saturday
-                    rday = DayOfWeek.Friday
-            End Select
-            Return rday
-        End Function
-
-        Private Function tdate(ByVal tday As DayOfWeek) As DayOfWeek
-            Dim rday As DayOfWeek
-            Select Case tday
-                Case DayOfWeek.Sunday
-                    rday = DayOfWeek.Monday
-                Case DayOfWeek.Monday
-                    rday = DayOfWeek.Tuesday
-                Case DayOfWeek.Tuesday
-                    rday = DayOfWeek.Wednesday
-                Case DayOfWeek.Wednesday
-                    rday = DayOfWeek.Thursday
-                Case DayOfWeek.Thursday
-                    rday = DayOfWeek.Friday
-                Case DayOfWeek.Friday
-                    rday = DayOfWeek.Saturday
-                Case DayOfWeek.Saturday
-                    rday = DayOfWeek.Sunday
-            End Select
-            Return rday
-        End Function
-
         Public Sub New()
             InitializeComponent()
+            _arrayButtons = New Button(5,6) {
+                                                {Su1, Mo1, Tu1, We1, Th1, Fr1, Sa1},
+                                                {Su2, Mo2, Tu2, We2, Th2, Fr2, Sa2},
+                                                {Su3, Mo3, Tu3, We3, Th3, Fr3, Sa3},
+                                                {Su4, Mo4, Tu4, We4, Th4, Fr4, Sa4},
+                                                {Su5, Mo5, Tu5, We5, Th5, Fr5, Sa5},
+                                                {Su6, Mo6, Tu6, We6, Th6, Fr6, Sa6}
+                                            }
             ReloadCal(Date.Today, Date.Today.Day)
         End Sub
 
