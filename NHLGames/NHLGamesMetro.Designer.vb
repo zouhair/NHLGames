d0b68a81eaf502759811de7e552cf354b9c4f2ee
jwallet <joseouellet@gmail.com>
2017-11-04 17:11:10 -0400

    download icons

--- NHLGames/NHLGamesMetro.Designer.vb
+++ NHLGames/NHLGamesMetro.Designer.vb
@@ -217,7 +217,7 @@ Partial Class NHLGamesMetro
         Me.tabMenu.Location = New System.Drawing.Point(-1, 60)
         Me.tabMenu.Margin = New System.Windows.Forms.Padding(0)
         Me.tabMenu.Name = "tabMenu"
-        Me.tabMenu.SelectedIndex = 1
+        Me.tabMenu.SelectedIndex = 0
         Me.tabMenu.Size = New System.Drawing.Size(992, 505)
         Me.tabMenu.SizeMode = System.Windows.Forms.TabSizeMode.Fixed
         Me.tabMenu.Style = MetroFramework.MetroColorStyle.Blue
@@ -1125,6 +1125,7 @@ Partial Class NHLGamesMetro
         Me.lnkGetMpc.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.lnkGetMpc.Cursor = System.Windows.Forms.Cursors.Hand
         Me.lnkGetMpc.ForeColor = System.Drawing.Color.Black
+        Me.lnkGetMpc.Image = Global.NHLGames.My.Resources.Resources.download
         Me.lnkGetMpc.Location = New System.Drawing.Point(655, 3)
         Me.lnkGetMpc.Name = "lnkGetMpc"
         Me.lnkGetMpc.Size = New System.Drawing.Size(25, 25)
@@ -1172,6 +1173,7 @@ Partial Class NHLGamesMetro
         Me.lnkGetVlc.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.lnkGetVlc.Cursor = System.Windows.Forms.Cursors.Hand
         Me.lnkGetVlc.ForeColor = System.Drawing.Color.Black
+        Me.lnkGetVlc.Image = Global.NHLGames.My.Resources.Resources.download
         Me.lnkGetVlc.Location = New System.Drawing.Point(655, 3)
         Me.lnkGetVlc.Name = "lnkGetVlc"
         Me.lnkGetVlc.Size = New System.Drawing.Size(25, 25)
